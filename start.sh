48f305e1e8d8aad25a6bc6be761c6afb393848b7
bajinho <bajo@live.jp>
2023-11-01 03:13:43 -0300

    :recycle:

--- start.sh
+++ start.sh
@@ -2,13 +2,15 @@
 
 mvn clean package
 
-cp $HOME/NetBeansProjects/biblioteca/target/biblioteca-1.0-SNAPSHOT.war $HOME/NetBeansProjects/biblioteca/docker
-
 cd $HOME/NetBeansProjects/biblioteca/docker
 
-docker build --tag=jboss/wildfly-admin .
+#yes | sudo rm -R workdir
+
+yes | cp $HOME/NetBeansProjects/biblioteca/target/biblioteca-1.0-SNAPSHOT.war $HOME/NetBeansProjects/biblioteca/docker
 
 docker-compose down
+
+docker build --tag=jboss/wildfly-admin .
 
 docker build --build-arg jdk=21 --tag=jboss/wildfly-admin .
 
